0b21dae1c393a23cfcb03bc13e4a0bc45bd8cdfa
reinforcexin <lixin510510@sina.com>
2016-12-14 00:27:50 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 public class Test{
 	public static void main(String[] args){
 		System.out.println("hello world");
+		for(int i = 0;i < 10;i++){
+			for(int j = 0;j < i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
